d903d935f7d9cca0b57aa6e89f8e69e5e9d1847a
Janette <janetterp2@gmail.com>
2026-03-18 17:31:57 +0100

    ex0

---
+++ ft_command_quest.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def receive_parameters() -> None:
+    if (len(sys.argv) < 2):
+        print("No arguments provided!")
+        print(f"Program name: {sys.argv[0]}")
+        print(f"Total arguments: {len(sys.argv)}")
+        return
+    else:
+        total_arguments = 0
+        for arg in sys.argv:
+            total_arguments += 1
+        total_arguments -= 1  # Minus the program name
+        print(f"Program name: {sys.argv[0]}")
+        print(f"Arguments received: {total_arguments}")
+        for arg in range(1, len(sys.argv)):  # arg serves as the counter 'i'
+            print(f"Argument {arg}: {sys.argv[arg]}")
+        print(f"Total arguments: {len(sys.argv)}")
+        return
+
+
+if __name__ == "__main__":
+    print("=== Command Quest ===")
+    receive_parameters()
