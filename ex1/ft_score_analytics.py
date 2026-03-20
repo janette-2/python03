0f134fba1a2be311e7911572d2c571e1cc09e00f
Janette <janetterp2@gmail.com>
2026-03-20 23:28:37 +0100

    new

--- ex1/ft_score_analytics.py
+++ ex1/ft_score_analytics.py
@@ -14,20 +14,22 @@ def scores_analysis() -> None:
               "<score1> <score2> ...")
         return
     i = 0
-    try:
-        for i in range(1, len(sys.argv)):
+    for i in range(1, len(sys.argv)):
+        try:
             scores += [int(sys.argv[i])]
-    except ValueError:
-        print(f"Invalid score provided: '{sys.argv[i]}'. Numbers will be the"
-              " only input accepted")
-        return
-    print(f"Scores processed: {scores}")
-    print(f"Total players: {len(scores)}")
-    print(f"Total score: {sum(scores)}")
-    print(f"Average score: {sum(scores)/len(scores)}")
-    print(f"High score: {max(scores)}")
-    print(f"Low score: {min(scores)}")
-    print(f"Score range: {max(scores) - min(scores)}")
+        except ValueError:
+            print(f"Invalid parameter: '{sys.argv[i]}'")
+    if not scores:
+        print("No scores provided. Usage: python3 ft_score_analytics.py "
+              "<score1> <score2> ...")
+    else:
+        print(f"Scores processed: {scores}")
+        print(f"Total players: {len(scores)}")
+        print(f"Total score: {sum(scores)}")
+        print(f"Average score: {sum(scores)/len(scores)}")
+        print(f"High score: {max(scores)}")
+        print(f"Low score: {min(scores)}")
+        print(f"Score range: {max(scores) - min(scores)}")
 
 
 if __name__ == "__main__":
