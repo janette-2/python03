191d4ceb0e08d9f76234a8dc2fa69b2e9cc88b89
Janette <janetterp2@gmail.com>
2026-03-21 01:23:48 +0100

    new

--- ex3/ft_achievement_tracker.py
+++ ex3/ft_achievement_tracker.py
@@ -9,48 +9,73 @@ def ft_achievement_tracker() -> None:
 
     # Declaring sets (list of unique data, without duplicates) == set(list[])
     # or set = {el1, el2, ...}
-    alice_set = set(["first_kill", "level_10", "treasure_hunter",
-                     "speed_demon"])
-    bob_set = set(["first_kill", "level_10", "boss_slayer", "collector"])
-    charlie_set = set(["level_10", "treasure_hunter", "boss_slayer",
-                      "speed_demon", "perfectionist"])
+    alice_set = set(["Crafting Genius", "World Savior", "Master Explorer",
+                     "Collector Supreme", "Untouchable", "Boss Slayer"])
+    charlie_set = set(["Strategist", "Speed Runner", "Survivor",
+                       "Master Explorer", "Treasure Hunter", "First Steps",
+                       "Collector Supreme", "Untouchable", "Sharp Mind"])
+    bob_set = set(["Crafting Genius", "Strategist", "World Savior",
+                   "Master Explorer", "Unstoppable", "Collector Supreme",
+                   "Untouchable"])
+    dylan_set = set(["Strategist", "Speed Runner", "Unstoppable",
+                     "Untouchable", "Boss Slayer"])
 
     # Printing sets (sets don't print the elements in order, it has no indexes)
     # sets work with hash (convertion of a text into an int and recognition of
     # each element through their int representation)
-    print(f"Player alice achievements:{alice_set}")
-    print(f"Player bob achievements:{bob_set}")
-    print(f"Player charlie achievements:{charlie_set}\n")
+    print(f"Player Alice:{alice_set}")
+    print(f"Player Bob:{bob_set}")
+    print(f"Player Charlie:{charlie_set}")
+    print(f"Player Dylan :{dylan_set}\n")
 
-    print("=== Achievement Analytics ===")
-    # Union
-    all_achieve = alice_set.union(bob_set).union(charlie_set)
-    print(f"All unique achievements: {all_achieve}")
-    print(f"Total unique achievements: {len(all_achieve)}\n")
+    # Union (aggregates everything)
+    all_achieve = alice_set.union(bob_set).union(charlie_set).union(dylan_set)
+    print(f"All unique achievements: {all_achieve}\n")
 
-    # Intersection
-    common = alice_set.intersection(bob_set).intersection(charlie_set)
-    print(f"Common to all players: {common}")
+    # Intersection (common to everyone)
+    common = alice_set.intersection(bob_set).intersection(
+        charlie_set).intersection(dylan_set)
+    print(f"Common to all players: {common}\n")
 
     # Difference
     # Unique achievements that only has each player
-    alice_only = alice_set.difference(bob_set).difference(charlie_set)
-    bob_only = bob_set.difference(alice_set).difference(charlie_set)
-    charlie_only = charlie_set.difference(alice_set).difference(bob_set)
-    # Union of all the achievements that only a player posses
-    uniques = alice_only.union(bob_only).union(charlie_only)
-    print(f"Rare achievements (1 player): {uniques}\n")
+    alice_only = alice_set.difference(bob_set).difference(
+        charlie_set).difference(dylan_set)
+    print(f"Only Alice has: {alice_only}")
+    bob_only = bob_set.difference(alice_set).difference(
+        charlie_set).difference(dylan_set)
+    print(f"Only Bob has: {bob_only}")
+    charlie_only = charlie_set.difference(alice_set).difference(
+        bob_set).difference(dylan_set)
+    print(f"Only Charlie has: {charlie_only}")
+    dylan_only = dylan_set.difference(alice_set).difference(
+        bob_set).difference(charlie_set)
+    print(f"Only Dylan has: {dylan_only}\n")
 
-    # Alice vs Bob
     #  Compare particular sets intersections
-    al_and_bob = alice_set.intersection(bob_set)
-    print(f"Alice vs Bob common: {al_and_bob}")
-    # Alice unique achivements
-    al_only = alice_set.difference(bob_set)
-    print(f"Alice unique: {al_only}")
-    # Bob unique achievements
-    bob_only = bob_set.difference(alice_set)
-    print(f"Bob unique: {bob_only}")
+    hidden_set = {"Hidden Path Finder"}
+
+    alice_missing = bob_set.difference(alice_set).union(charlie_set.difference(
+        alice_set)).union(dylan_set.difference(alice_set)).union(
+            hidden_set.difference(alice_set))
+    print(f"Alice is missing: {alice_missing}")
+
+    bob_missing = alice_set.difference(bob_set).union(charlie_set.difference(
+        bob_set)).union(dylan_set.difference(bob_set)).union(
+            hidden_set.difference(bob_set))
+    print(f"Bob is missing: {bob_missing}")
+
+    charlie_missing = alice_set.difference(charlie_set).union(
+                    bob_set.difference(charlie_set)).union(
+                    dylan_set.difference(charlie_set)).union(
+                    hidden_set.difference(charlie_set))
+    print(f"Charlie is missing: {charlie_missing}")
+
+    dylan_missing = alice_set.difference(dylan_set).union(
+                    bob_set.difference(dylan_set)).union(
+                    charlie_set.difference(dylan_set)).union(
+                    hidden_set.difference(dylan_set))
+    print(f"Charlie is missing: {dylan_missing}")
 
 
 if __name__ == "__main__":
